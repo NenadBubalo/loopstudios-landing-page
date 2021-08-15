fe3c8d9f0a2f687f35737d173fb67df4660d2c20
nenad.bubalo@students.ffhs.ch <nenad.bubalo@students.ffhs.ch>
2021-08-15 21:53:39 +0200

    Release 15.08.21 part 2

---
+++ src/code.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+document.getElementById("button").onclick = function () {
+    count++;
+    if (count % 2 === 0) {
+        document.getElementById("main__creationsImg-underDiv").style.display = "grid";
+        document.getElementById("button").style.backgroundColor = "hsl(0, 0%, 100%)";
+        document.getElementById("button__text").style.color = "hsl(0, 0%, 0%)";
+    }
+    else {
+        document.getElementById("main__creationsImg-underDiv").style.display = "none";
+        document.getElementById("button").style.backgroundColor = "hsl(0, 0%, 0%)";
+        document.getElementById("button__text").style.color = "hsl(0, 0%, 100%)";
+    }
+}
